4e3bc027cbacbc60413d14e5b49e77570e77022f
tobiasalanboyd <eboyd@udel.edu>
2019-10-30 17:19:10 -0400

    Added user delete, edit, and view functions

--- db/usercontroller.js
+++ db/usercontroller.js
@@ -18,7 +18,7 @@ exports.index = function (req, res) {
     });
 };
 
-//Make a new user
+//Add a user
 
 exports.new = function (req, res) {
     var user = new User();
@@ -39,7 +39,53 @@ res.json({
     });
 };
 
+//Delete a user by id
+exports.delete = function (req, res) {
+    User.remove({
+        _id: req.params._id
+    }, function (err, user) {
+        if (err)
+            res.send(err);
+res.json({
+            status: "success",
+            message: 'User deleted'
+        });
+    });
+};
 
+//Find user by id
+exports.view = function (req, res) {
+    User.findById(req.params._id, function (err, user) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'User details:',
+            data: user
+        });
+    });
+};
 
+// Edit user by id
+exports.update = function (req, res) {
+    User.findById(req.params._id, function (err, user) {
+            if (err)
+                res.send(err);
+    user._id = req.body._id;
+    user.firstName = req.body.firstName ? req.body.firstName : user.firstName;
+    user.lastName = req.body.lastName ? req.body.lastName : user.lastName;
+    user.email = req.body.email;
+    user.role = req.body.role;
+    user.provider = req.body.provider;
+    // save the user and check for errors
+            user.save(function (err) {
+                if (err)
+                    res.json(err);
+                res.json({
+                    message: 'User Info updated',
+                    data: user
+                });
+            });
+        });
+    };
 
 
